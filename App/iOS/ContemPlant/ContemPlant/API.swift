eceb617361068adee2bbdc2435c7839a4608948f
Gero <gero.embser@gmail.com>
2018-06-20 22:18:16 +0200

    Update queries-file because Prisma has problems with Fragments in subscriptions
    See corresponding github issue

--- App/iOS/ContemPlant/ContemPlant/API.swift
+++ App/iOS/ContemPlant/ContemPlant/API.swift
@@ -2,9 +2,48 @@
 
 import Apollo
 
+public enum MutationType: RawRepresentable, Equatable, Apollo.JSONDecodable, Apollo.JSONEncodable {
+  public typealias RawValue = String
+  case created
+  case updated
+  case deleted
+  /// Auto generated constant for unknown enum values
+  case __unknown(RawValue)
+
+  public init?(rawValue: RawValue) {
+    switch rawValue {
+      case "CREATED": self = .created
+      case "UPDATED": self = .updated
+      case "DELETED": self = .deleted
+      default: self = .__unknown(rawValue)
+    }
+  }
+
+  public var rawValue: RawValue {
+    switch self {
+      case .created: return "CREATED"
+      case .updated: return "UPDATED"
+      case .deleted: return "DELETED"
+      case .__unknown(let value): return value
+    }
+  }
+
+  public static func == (lhs: MutationType, rhs: MutationType) -> Bool {
+    switch (lhs, rhs) {
+      case (.created, .created): return true
+      case (.updated, .updated): return true
+      case (.deleted, .deleted): return true
+      case (.__unknown(let lhsValue), .__unknown(let rhsValue)): return lhsValue == rhsValue
+      default: return false
+    }
+  }
+}
+
 public final class PlantsQuery: GraphQLQuery {
-  public static let operationString =
-    "query Plants {\n  plants {\n    __typename\n    id\n    name\n  }\n}"
+  public let operationDefinition =
+    "query Plants {\n  plants {\n    __typename\n    ...BasicPlantDetails\n  }\n}"
+
+  public var queryDocument: String { return operationDefinition.appending(BasicPlantDetails.fragmentDefinition) }
 
   public init() {
   }
@@ -16,22 +55,22 @@ public final class PlantsQuery: GraphQLQuery {
       GraphQLField("plants", type: .nonNull(.list(.nonNull(.object(Plant.selections))))),
     ]
 
-    public var snapshot: Snapshot
+    public private(set) var resultMap: ResultMap
 
-    public init(snapshot: Snapshot) {
-      self.snapshot = snapshot
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
     }
 
     public init(plants: [Plant]) {
-      self.init(snapshot: ["__typename": "Query", "plants": plants.map { (value: Plant) -> Snapshot in value.snapshot }])
+      self.init(unsafeResultMap: ["__typename": "Query", "plants": plants.map { (value: Plant) -> ResultMap in value.resultMap }])
     }
 
     public var plants: [Plant] {
       get {
-        return (snapshot["plants"] as! [Snapshot]).map { (value: Snapshot) -> Plant in Plant(snapshot: value) }
+        return (resultMap["plants"] as! [ResultMap]).map { (value: ResultMap) -> Plant in Plant(unsafeResultMap: value) }
       }
       set {
-        snapshot.updateValue(newValue.map { (value: Plant) -> Snapshot in value.snapshot }, forKey: "plants")
+        resultMap.updateValue(newValue.map { (value: Plant) -> ResultMap in value.resultMap }, forKey: "plants")
       }
     }
 
@@ -40,46 +79,252 @@ public final class PlantsQuery: GraphQLQuery {
 
       public static let selections: [GraphQLSelection] = [
         GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
         GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
         GraphQLField("name", type: .nonNull(.scalar(String.self))),
       ]
 
-      public var snapshot: Snapshot
+      public private(set) var resultMap: ResultMap
 
-      public init(snapshot: Snapshot) {
-        self.snapshot = snapshot
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
       }
 
       public init(id: GraphQLID, name: String) {
-        self.init(snapshot: ["__typename": "Plant", "id": id, "name": name])
+        self.init(unsafeResultMap: ["__typename": "Plant", "id": id, "name": name])
       }
 
       public var __typename: String {
         get {
-          return snapshot["__typename"]! as! String
+          return resultMap["__typename"]! as! String
         }
         set {
-          snapshot.updateValue(newValue, forKey: "__typename")
+          resultMap.updateValue(newValue, forKey: "__typename")
         }
       }
 
       public var id: GraphQLID {
         get {
-          return snapshot["id"]! as! GraphQLID
+          return resultMap["id"]! as! GraphQLID
         }
         set {
-          snapshot.updateValue(newValue, forKey: "id")
+          resultMap.updateValue(newValue, forKey: "id")
         }
       }
 
       public var name: String {
         get {
-          return snapshot["name"]! as! String
+          return resultMap["name"]! as! String
         }
         set {
-          snapshot.updateValue(newValue, forKey: "name")
+          resultMap.updateValue(newValue, forKey: "name")
         }
       }
+
+      public var fragments: Fragments {
+        get {
+          return Fragments(unsafeResultMap: resultMap)
+        }
+        set {
+          resultMap += newValue.resultMap
+        }
+      }
+
+      public struct Fragments {
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public var basicPlantDetails: BasicPlantDetails {
+          get {
+            return BasicPlantDetails(unsafeResultMap: resultMap)
+          }
+          set {
+            resultMap += newValue.resultMap
+          }
+        }
+      }
+    }
+  }
+}
+
+public final class NewPlantsSubscription: GraphQLSubscription {
+  public let operationDefinition =
+    "subscription NewPlants {\n  newPlant {\n    __typename\n    mutation\n    node {\n      __typename\n      id\n      name\n    }\n  }\n}"
+
+  public init() {
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes = ["Subscription"]
+
+    public static let selections: [GraphQLSelection] = [
+      GraphQLField("newPlant", type: .object(NewPlant.selections)),
+    ]
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(newPlant: NewPlant? = nil) {
+      self.init(unsafeResultMap: ["__typename": "Subscription", "newPlant": newPlant.flatMap { (value: NewPlant) -> ResultMap in value.resultMap }])
+    }
+
+    public var newPlant: NewPlant? {
+      get {
+        return (resultMap["newPlant"] as? ResultMap).flatMap { NewPlant(unsafeResultMap: $0) }
+      }
+      set {
+        resultMap.updateValue(newValue?.resultMap, forKey: "newPlant")
+      }
+    }
+
+    public struct NewPlant: GraphQLSelectionSet {
+      public static let possibleTypes = ["PlantSubscriptionPayload"]
+
+      public static let selections: [GraphQLSelection] = [
+        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+        GraphQLField("mutation", type: .nonNull(.scalar(MutationType.self))),
+        GraphQLField("node", type: .object(Node.selections)),
+      ]
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(mutation: MutationType, node: Node? = nil) {
+        self.init(unsafeResultMap: ["__typename": "PlantSubscriptionPayload", "mutation": mutation, "node": node.flatMap { (value: Node) -> ResultMap in value.resultMap }])
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      public var mutation: MutationType {
+        get {
+          return resultMap["mutation"]! as! MutationType
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "mutation")
+        }
+      }
+
+      public var node: Node? {
+        get {
+          return (resultMap["node"] as? ResultMap).flatMap { Node(unsafeResultMap: $0) }
+        }
+        set {
+          resultMap.updateValue(newValue?.resultMap, forKey: "node")
+        }
+      }
+
+      public struct Node: GraphQLSelectionSet {
+        public static let possibleTypes = ["Plant"]
+
+        public static let selections: [GraphQLSelection] = [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
+          GraphQLField("name", type: .nonNull(.scalar(String.self))),
+        ]
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public init(id: GraphQLID, name: String) {
+          self.init(unsafeResultMap: ["__typename": "Plant", "id": id, "name": name])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        public var id: GraphQLID {
+          get {
+            return resultMap["id"]! as! GraphQLID
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "id")
+          }
+        }
+
+        public var name: String {
+          get {
+            return resultMap["name"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "name")
+          }
+        }
+      }
+    }
+  }
+}
+
+public struct BasicPlantDetails: GraphQLFragment {
+  public static let fragmentDefinition =
+    "fragment BasicPlantDetails on Plant {\n  __typename\n  id\n  name\n}"
+
+  public static let possibleTypes = ["Plant"]
+
+  public static let selections: [GraphQLSelection] = [
+    GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+    GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
+    GraphQLField("name", type: .nonNull(.scalar(String.self))),
+  ]
+
+  public private(set) var resultMap: ResultMap
+
+  public init(unsafeResultMap: ResultMap) {
+    self.resultMap = unsafeResultMap
+  }
+
+  public init(id: GraphQLID, name: String) {
+    self.init(unsafeResultMap: ["__typename": "Plant", "id": id, "name": name])
+  }
+
+  public var __typename: String {
+    get {
+      return resultMap["__typename"]! as! String
+    }
+    set {
+      resultMap.updateValue(newValue, forKey: "__typename")
+    }
+  }
+
+  public var id: GraphQLID {
+    get {
+      return resultMap["id"]! as! GraphQLID
+    }
+    set {
+      resultMap.updateValue(newValue, forKey: "id")
+    }
+  }
+
+  public var name: String {
+    get {
+      return resultMap["name"]! as! String
+    }
+    set {
+      resultMap.updateValue(newValue, forKey: "name")
     }
   }
 }
