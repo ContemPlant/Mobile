155452012c1aab974a5d1f062d4ed9ad83d1cdde
Gero <gero.embser@gmail.com>
2018-07-02 13:24:58 +0200

    update for HashRouter

--- App/React.nosync/ContemPlant/app/config/Constants.js
+++ App/React.nosync/ContemPlant/app/config/Constants.js
@@ -1,6 +1,6 @@
 export const ServerConstants = new function() {
     this.baseURL = "http://167.99.240.197",
-    this.loginURL = this.baseURL+":3000/login"
-    this.overviewURL = this.baseURL+":3000/overview"
+    this.loginURL = this.baseURL+":8080/#/login"
+    this.overviewURL = this.baseURL+":8080/#/overview"
     this.graphQLURL = this.baseURL+":8000/graphql"
 }
