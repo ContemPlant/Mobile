f27ec775e26456a01df370a30f1528efae61d215
Gero <gero.embser@gmail.com>
2018-07-02 03:34:49 +0200

    update server url to digitalOcean

--- App/React.nosync/ContemPlant/app/config/Constants.js
+++ App/React.nosync/ContemPlant/app/config/Constants.js
@@ -1,5 +1,5 @@
 export const ServerConstants = new function() {
-    this.baseURL = "http://192.168.178.20",
+    this.baseURL = "http://167.99.240.197",
     this.loginURL = this.baseURL+":3000/login"
     this.overviewURL = this.baseURL+":3000/overview"
     this.graphQLURL = this.baseURL+":8000/graphql"
